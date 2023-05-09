204ece8133ce4684911b892585e99d0e1c307e75
Thomas Deruere <t.deruere@gmail.com>
2023-05-09 10:44:20 +0200

    tests modelviewset et filtres sur objets Issue ok

--- softdesk/api/views.py
+++ softdesk/api/views.py
@@ -2,8 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 from rest_framework.views import APIView
-from rest_framework.viewsets import ModelViewSet
-
+from rest_framework.viewsets import ModelViewSet, ReadOnlyModelViewSet
 from rest_framework.response import Response
 
 from api.models import Project, User, Issue
@@ -16,15 +15,22 @@ class UserAPIView(APIView):
         users = User.objects.all()
         serializer = UserSerializer(users, many=True)
         return Response(serializer.data)
-class ProjectAPIView(APIView):
 
-    def get(self, *args, **kwargs):
-        projects = Project.objects.all()
-        serializer = ProjectSerializer(projects, many=True)
-        return Response(serializer.data)
+class ProjectViewset(ReadOnlyModelViewSet):
+
+    serializer_class = ProjectSerializer
+    def get_queryset(self):
+        return Project.objects.all()
 
 class IssueViewset(ModelViewSet):
 
     serializer_class = IssueSerializer
     def get_queryset(self):
-        return Issue.objects.all()
+        queryset = Issue.objects.filter(status="active")
+        # http://127.0.0.1:8000/api/issue/?priority=1
+        project_id = self.request.GET.get('project_id')
+        if project_id:
+            queryset = queryset.filter(project_id=project_id)
+        return queryset
+    # def perform_create(self, serializer):
+    #     serializer.save(user=self.request.user)
--- softdesk/softdesk/urls.py
+++ softdesk/softdesk/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from api.views import ProjectAPIView, UserAPIView, IssueViewset
+from api.views import UserAPIView, ProjectViewset, IssueViewset
 
 from rest_framework import routers
 
@@ -25,12 +25,12 @@ router = routers.SimpleRouter()
 # Puis lui déclarons une url basée sur le mot clé ‘category’ et notre view
 # afin que l’url générée soit celle que nous souhaitons ‘/api/category/’
 router.register('issue', IssueViewset, basename='issue')
+router.register('project', ProjectViewset, basename='project')
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
-    path('api/project/', ProjectAPIView.as_view()),
     path('api/user/', UserAPIView.as_view()),
     path('api/', include(router.urls))  # Il faut bien penser à ajouter les urls du router dans la liste des urls disponibles.
 ]
